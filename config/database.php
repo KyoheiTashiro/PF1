1218073a11127b6f621f2d960432bdcc107b1279
Kyohei Tashiro <kyokyo_dc5@yahoo.co.jp>
2022-11-08 20:51:08 +0900

    テスト

--- config/database.php
+++ config/database.php
@@ -106,8 +106,8 @@ return [
         'sqlite_testing' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
-            // 'database' => env('DB_DATABASE', storage_path('testing.sqlite')),
-            'database' => env('DB_DATABASE', database_path('testing.sqlite')),
+            'database' => env('DB_DATABASE', storage_path('testing.sqlite')),
+            // 'database' => env('DB_DATABASE', database_path('testing.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
